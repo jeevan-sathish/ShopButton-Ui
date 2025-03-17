900dfa1b85efb38468443a386662f5679e06d059
Jeevan s <aryajeevan68@gmail.com>
2025-03-17 22:02:22 +0530

    change done

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -19,6 +19,7 @@ const NotFound = () => {
         <a href="/" className="text-blue-500 hover:text-blue-700 underline">
           Return to Home
         </a>
+        
       </div>
     </div>
   );
